54fb2f623539ff13642a96eecd3d04dd430c0408
JulietaPaoloni <paolonijulieta@gmail.com>
2024-09-12 12:16:41 -0300

    Initial commit

---
+++ src/main/java/Config/CustomRevisionListener.java
@@ -0,0 +1,11 @@
+package Config;
+import Audit.Revision;
+import org.hibernate.envers.RevisionListener;
+
+public class CustomRevisionListener  implements RevisionListener {
+    @Override
+    public void newRevision(Object revisionEntity) {
+        final Revision revision = (Revision) revisionEntity;
+
+    }
+}
---
+++ src/main/java/Entidades/Categoria.java
@@ -0,0 +1,36 @@
+package Entidades;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Audited
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "denominacion")
+    private String denominacion;
+
+    @ManyToMany(mappedBy = "categorias")
+    private List<Articulo> articulos = new ArrayList<>();
+
+    // Constructor que acepta solo el nombre (denominacion)
+    public Categoria(String denominacion) {
+        this.denominacion = denominacion;
+        this.articulos = new ArrayList<>();
+    }
+}
